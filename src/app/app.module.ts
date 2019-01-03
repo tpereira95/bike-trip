d80f00175ec2280bb40328b87fa0689017a8d72f
Unknown <tp_ca@yahoo.com>
2019-01-03 17:48:46 -0500

    Project initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TripCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
